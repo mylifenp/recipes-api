ef04305d03fadcd04b9903729962c5f557caed93
Amit Shah <mylifenp@yahoo.com>
2022-09-07 22:03:10 +0200

    adding swagger

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"github.com/mylifenp/recipes-api/docs"
 	"github.com/mylifenp/recipes-api/routes"
+	docs "github.com/recipes-api/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io:8080
+// @BasePath /v2
 
 func main() {
 	router := routes.SetupRouter()
-	router.Run("localhost:8080")
+	docs.SwaggerInfo.BasePath = "/"
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+	router.Run()
 }
